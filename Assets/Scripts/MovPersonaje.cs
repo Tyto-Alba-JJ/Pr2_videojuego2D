be59a56f75c7f660c492336014ff649593c1e6c7
TytoAlba <Tytoalbajjfm@gmail.com>
2025-04-29 14:48:19 +0200

    Cambios videos hasta salto (6)

---
+++ Assets/Scripts/MovPersonaje.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovPersonaje : MonoBehaviour
+{
+public float multiplicador = 5f;
+
+public float multiplicadorSalto = 5f;
+
+private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movTeclas = Input.GetAxis("Horizontal");
+
+        float miDeltaTime = Time.deltaTime;
+
+        Debug.Log(Time.deltaTime);
+
+        transform.Translate(
+            movTeclas*(Time.deltaTime*multiplicador),
+            0,
+            0
+        );
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rb.AddForce(
+                new Vector2(0,multiplicadorSalto),
+                ForceMode2D.Impulse
+            );
+        }
+    }
+}

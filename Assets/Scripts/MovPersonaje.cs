709aaae4d1f63705cf8cc96a25a6507e0feb27da
TytoAlba <Tytoalbajjfm@gmail.com>
2025-04-29 19:28:22 +0200

    Movmiento Y salto del personaje revisado

--- Assets/Scripts/MovPersonaje.cs
+++ Assets/Scripts/MovPersonaje.cs
@@ -1,41 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MovPersonaje : MonoBehaviour
 {
+public float velocidad = 5f;
 public float multiplicador = 5f;
 
 public float multiplicadorSalto = 5f;
 
+private bool puedoSaltar = true;
+
 private Rigidbody2D rb;
+
+private Animator animatorController;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = this.GetComponent<Rigidbody2D>();
+
+        animatorController = this.GetComponent<Animator>();
+
+        transform.position = new Vector3(-3.1f, -1.0f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movTeclas = Input.GetAxis("Horizontal");
-
         float miDeltaTime = Time.deltaTime;
 
-        Debug.Log(Time.deltaTime);
+        float movTeclas = Input.GetAxis("Horizontal");
+        
+        rb.velocity = new Vector2(movTeclas*multiplicador, rb.velocity.y);
 
-        transform.Translate(
-            movTeclas*(Time.deltaTime*multiplicador),
-            0,
-            0
-        );
+        if(movTeclas < 0){
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }else if(movTeclas > 0){
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
 
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(movTeclas != 0){
+            animatorController.SetBool("activacamina", true);
+        }else{
+            animatorController.SetBool("activacamina", false);
+        }
+
+       RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f);
+       Debug.DrawRay(transform.position, Vector2.down, Color.white);
+
+        if (hit){
+            puedoSaltar = true;
+        }else{
+            puedoSaltar = false;
+        };
+
+        if(Input.GetKeyDown(KeyCode.Space) && puedoSaltar){
             rb.AddForce(
                 new Vector2(0,multiplicadorSalto),
                 ForceMode2D.Impulse
             );
+            //puedoSaltar = false;
         }
+    }
+
+    void OnCollisionEnter2D(){
+        puedoSaltar = true;
     }
 }
